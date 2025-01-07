12cb87e5cbb1fdc6549f4ee4036a3d858a4eaf37
lucithub <lucasvelez2013@gmail.com>
2025-01-07 20:02:23 -0300

    Ejercicio 1 en C# hecho

---
+++ C#/Ejercicio 1/Ejercicio1/Program.cs
@@ -0,0 +1,11 @@
+﻿string[] nombres = new string[8];
+for (int i = 0; i < nombres.Length; i++)
+{
+    Console.WriteLine("Ingrese un nombre: ");
+    nombres[i] = Console.ReadLine();
+}
+Console.WriteLine("Los nombres que ha elegido son los siguientes");
+foreach (string nombre in nombres)
+{
+    Console.WriteLine(nombre);
+}
